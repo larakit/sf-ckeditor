10eb040e0f910c26420f57ba7d56b4a79d8833e8
Berdnikov Alexey <aberdnikov@gmail.com>
2016-07-04 17:22:41 +0500

    Create init.php

---
+++ init.php
@@ -0,0 +1,4 @@
+<?php
+\Larakit\StaticFiles\Manager::package('larakit/sf-ckeditor')
+  ->js('//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.5.9/ckeditor.js')
+  ->js('//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.5.9/config.js');
