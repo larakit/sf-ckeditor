dd2c7c4518fd5431b4f487b884581d810d7591d1
Alexey Berdnikov <aberdnikov@gmail.com>
2016-08-07 21:38:30 +0500

    fixed

--- init.php
+++ init.php
@@ -1,5 +1,9 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-ckeditor')
-    ->setSourceDir('public')
-    ->jsPackage('ckeditor.js')
-    ->jsPackage('config.js');
+    ->js('//cdn.ckeditor.com/4.5.10/standard/ckeditor.js')
+    ->jsPackage('laraform-wysiwyg.js');
+//\Larakit\StaticFiles\Manager::package('larakit/sf-ckeditor')
+//    ->setSourceDir('public')
+//    ->jsPackage('ckeditor.js')
+//    ->jsPackage('config.js')
+//    ->jsPackage('laraform-wysiwyg.js');
