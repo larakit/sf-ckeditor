752e0ca46b8e0a7d8ea0cee87f3c0a8ca50d2aec
Alexey Berdnikov <aberdnikov@gmail.com>
2018-02-19 03:02:55 +0500

    fixed

--- init.php
+++ init.php
@@ -2,7 +2,7 @@
 \Larakit\StaticFiles\Manager::package('larakit/sf-ckeditor')
     ->setSourceDir('public')
     ->usePackage('larakit/sf-jquery')
-    ->js('//cdn.ckeditor.com/4.5.10/standard/ckeditor.js')
+//    ->js('//cdn.ckeditor.com/4.5.10/standard/ckeditor.js')
     ->jsPackage('ckeditor.js')
 ;
 //\Larakit\StaticFiles\Manager::package('larakit/sf-ckeditor')
